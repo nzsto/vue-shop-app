5515eef0efd9cd6f394cb532976f93ca3936cdd9
liuyq <657791173@qq.com>
2019-06-06 08:43:22 +0800

    每日打卡

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,5 +1,5 @@
 <template>
-     <div class="content">
+    <div class="content">
         <dl class="search-step">
             <dt class="search-content">
                 三步轻松获得优惠券
@@ -30,33 +30,35 @@
             </dd>
         </dl>
         <div class="hr"></div>
-        
+
     </div>
 </template>
 
 <script>
-export default {
-    data(){
-        return {
-            icon:{
-                left:'&#xe617;',
-                center:'&#xe616;',
-                right:'&#xe610;'
+    export default {
+        data() {
+            return {
+                icon: {
+                    left: '&#xe617;',
+                    center: '&#xe616;',
+                    right: '&#xe610;'
+                }
             }
         }
     }
-}
 </script>
 
 <style scoped>
-    .iconfont{
-      color:#fff;
-      font-size: .6rem;  
+    .iconfont {
+        color: #fff;
+        font-size: .6rem;
     }
-    .content{
-        padding-top:.9rem;
+
+    .content {
+        padding-top: .9rem;
     }
-    .content>.search-step>.search-content{
+
+    .content>.search-step>.search-content {
         height: 1rem;
         line-height: .8rem;
         font-size: .3rem;
@@ -64,47 +66,54 @@ export default {
         width: 100%;
         color: #999;
     }
-    .content>.search-step>dd{
+
+    .content>.search-step>dd {
         display: flex;
-        justify-content:center;
+        justify-content: center;
         position: relative;
     }
-    .content>.search-step>dd>.xian,.content>.search-step>dd>.xian1{
+
+    .content>.search-step>dd>.xian,
+    .content>.search-step>dd>.xian1 {
         width: 21%;
         height: .05rem;
         display: block;
         position: absolute;
-        top: .7rem;       
+        top: .7rem;
         opacity: 0.5;
-        background: linear-gradient(to left,#FF83B7 0,#FFAF98 100%);
+        background: linear-gradient(to left, #FF83B7 0, #FFAF98 100%);
     }
-    .content>.search-step>dd>.xian{
+
+    .content>.search-step>dd>.xian {
         left: 1.7rem;
     }
-    .content>.search-step>dd>.xian1{
+
+    .content>.search-step>dd>.xian1 {
         left: 4.24rem;
     }
-    .content>.search-step>dd>.search-box>i{
+
+    .content>.search-step>dd>.search-box>i {
         width: 1rem;
         height: 1rem;
         display: block;
         border-radius: 50%;
         text-align: center;
-        background: linear-gradient(to left,#FF83B7 0,#FFAF98 100%);
+        background: linear-gradient(to left, #FF83B7 0, #FFAF98 100%);
         margin: auto auto .1rem;
-        margin-top:.2rem;
+        margin-top: .2rem;
         margin-bottom: .5rem;
     }
-    .content>.search-step>dd>.search-box>p{
+
+    .content>.search-step>dd>.search-box>p {
         margin: 0 .3rem;
         text-align: center;
         font-size: .2rem;
         color: #555;
         margin-bottom: .4rem;
     }
+
     .hr {
         background: #F5F5F5;
         height: .2rem;
     }
-
 </style>
