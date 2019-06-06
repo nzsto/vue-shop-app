f9cb5f9646247164eec1cc1e0c92a262409877fa
pananing <1131542635@qq.com>
2019-06-06 16:11:35 +0800

    0

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-require('./mock/mockbase')
 import Observer from "./Observer"
 import router from "./routers"
 import store from "./store"
 import 'swiper/dist/css/swiper.css';
+import BScroll from "@common/BScroll/BScroll"
+require('./mock/mockbase')
 Vue.config.productionTip = false
 import VueTouch from 'vue-touch'
+Vue.component("BScroll",BScroll);
 Vue.use(VueTouch, {name: 'v-touch'})
 new Vue({
   router,
